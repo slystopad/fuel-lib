c0b27bc9ed68e4fc0e5b17e704e7de0946a74a5c
Sergiy Lystopad <slystopad@mirantis.com>
2014-11-18 13:20:49 +0200

    Add HAProxy stuff for LDAP server.

---
+++ deployment/puppet/openstack/manifests/ha/ldap.pp
@@ -0,0 +1,19 @@
+# HA configuration for LDAP server which lives on controller and populated by scripts
+# from parent LDAP server
+class openstack::ha::ldap {
+
+  openstack::ha::haproxy_service { 'ldap':
+    order           => '389',
+    listen_port     => 389,
+    public          => false,
+    #require_service => 'slapd',
+    mode            => 'tcp',
+  }
+    haproxy_config_options => {
+      'option'  => ['socket-stats', 'tcpka'],
+      'timeout' => ['client 5s', 'server 2s', 'connect 1s'],
+    },
+    
+    balancermember_options => 'check fall 1 rise 1 inter 2s',
+
+}
