2f9e0a1bc3bdb56e562ff5a19c73571e04e47b11
Sergiy Lystopad <slystopad@mirantis.com>
2014-12-01 14:56:33 +0200

    Reduce number of attributes for Keystone LDAP backend obrained via UI and configured in keystone.conf.

--- deployment/puppet/keystone/manifests/config/ldap.pp
+++ deployment/puppet/keystone/manifests/config/ldap.pp
@@ -36,20 +36,15 @@ class keystone::ldap(
 
 class keystone::config::ldap(
   $url            = $::fuel_settings['keystone_ldap']['ldap_url'],
+  $suffix         = $::fuel_settings['keystone_ldap']['ldap_suffix'],
   $user           = $::fuel_settings['keystone_ldap']['ldap_user'],
   $password       = $::fuel_settings['keystone_ldap']['ldap_pass'],
-  $suffix         = $::fuel_settings['keystone_ldap']['ldap_suffix'],
   $user_tree_dn   = $::fuel_settings['keystone_ldap']['ldap_user_tree_dn'],
-  $tenant_tree_dn = $::fuel_settings['keystone_ldap']['ldap_tenant_tree_dn'],
-  $role_tree_dn   = $::fuel_settings['keystone_ldap']['ldap_role_tree_dn'],
-  $group_tree_dn   = $::fuel_settings['keystone_ldap']['ldap_group_tree_dn'],
-  $user_filter    = $::fuel_settings['keystone_ldap']['ldap_user_filter'],
-  $role_filter    = $::fuel_settings['keystone_ldap']['ldap_role_filter'],
-  $tenant_filter  = $::fuel_settings['keystone_ldap']['ldap_tenant_filter'],
-  $tenant_id_prefix = $::fuel_settings['keystone_ldap']['ldap_tenant_id_prefix'],
-  $tenant_name_prefix = $::fuel_settings['keystone_ldap']['ldap_tenant_name_prefix'],
-  $role_id_prefix = $::fuel_settings['keystone_ldap']['ldap_role_id_prefix'],
-  $role_name_prefix = $::fuel_settings['keystone_ldap']['ldap_role_name_prefix'],
+  $user_objectclass    = $::fuel_settings['keystone_ldap']['ldap_user_objectclass'],
+  $user_id_attribute   = $::fuel_settings['keystone_ldap']['ldap_user_id_attribute'],
+  $user_mail_attribute = $::fuel_settings['keystone_ldap']['ldap_user_mail_attribute'],
+  $user_pass_attribute = $::fuel_settings['keystone_ldap']['ldap_user_pass_attribute'],
+  $user_enabled_attribute = $::fuel_settings['keystone_ldap']['ldap_user_enabled_attribute'],
   $ldap_driver    = 'keystone.identity.backends.ldap.Identity',
 ) {
 
@@ -60,27 +55,16 @@ class keystone::config::ldap(
   }
 
   keystone_config {
-    'ldap/url':            value => $url;
-    'ldap/user':           value => "'${user}'";
-    'ldap/password':       value => $password;
-    'ldap/suffix':         value => $suffix;
-    'ldap/user_tree_dn':   value => "'${user_tree_dn}'";
-    'ldap/tenant_tree_dn': value => "'${tenant_tree_dn}'";
-    'ldap/role_tree_dn':   value => "'${role_tree_dn}'";
-    'ldap/user_filter':    value => $user_filter;
-    'ldap/role_filter':    value => $role_filter;
-    'ldap/tenant_filter':  value => $tenant_filter;
-    'ldap/group_tree_dn':  value => "'${group_tree_dn}'";
-    'ldap/query_scope':    value => "sub";
-    'identity/driver':     value => $ldap_driver;
-    #"ldap/tree_dn" value => "dc=example,dc=com",
-    'ldap/role_id_prefix':   value => $role_id_prefix;
-    'ldap/tenant_id_prefix': value => $tenant_id_prefix;
-    'ldap/role_name_prefix':   value => $role_name_prefix;
-    'ldap/tenant_name_prefix': value => $tenant_name_prefix;
-    #
-    'ldap/user_allow_create':       value => "False";
-    'ldap/user_allow_update':       value => "False";
-    'ldap/user_allow_delete':       value => "False";
+    'ldap/url':				value => $url;
+    'ldap/suffix':			value => $suffix;
+    'ldap/user':			value => "'${user}'";
+    'ldap/password':			value => $password;
+    'ldap/user_tree_dn':		value => "'${user_tree_dn}'";
+    'ldap/user_objectclass':		value => "'${user_objectclass}'";
+    'ldap/user_id_attribute':		value => "'${user_id_attribute}'";
+    'ldap/user_mail_attribute':		value => "'${user_mail_attribute}'";
+    'ldap/user_pass_attribute':		value => "'${user_pass_attribute}'";
+    'ldap/user_enabled_attribute':	value => "'${user_enabled_attribute}'";
+    'identity/driver':			value => $ldap_driver;
   }
 }
